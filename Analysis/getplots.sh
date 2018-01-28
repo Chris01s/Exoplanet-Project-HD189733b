3919faa7932da0c3a723162b172104a4063c8b80
Chris01s <cowens21@qub.ac.uk>
2018-01-28 18:05:29 +0000

    removed

--- Analysis/getplots.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#open plots
-FILES=$(ls plots/)
-
-for f in $FILES
-do
-	xdg-open plots/$f
-done
--- Scripts/move_fits_files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mv *.fits /home/chris/Documents/Exoplanet_Research/HD189733/pro_path/
-
